181e970d0a06fae42f84445e6d496d4ff9a7af0b
ahmed-abdelsalam <engahmedabdelsalam918@gmail.com>
2018-08-09 01:04:41 +0200

    Initial commit

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -15,14 +15,59 @@ using System.Windows.Shapes;
 
 namespace WpfApp2
 {
+    using System.Windows.Media;
+
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        int cellindex = 1;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (cellindex == 3)
+                cellindex = 1;
+            GridLength heigh = new GridLength(290);
+            RowDefinition newrow = new RowDefinition();
+            newrow.Height = heigh;
+            dashboard.RowDefinitions.Add(newrow);
+
+
+
+            GridSplitter gs = new GridSplitter();
+            gs.VerticalAlignment =System.Windows.VerticalAlignment.Bottom;
+            gs.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+            gs.Height = 8;
+            gs.ResizeDirection = System.Windows.Controls.GridResizeDirection.Rows;
+            gs.BorderThickness= new Thickness(3);
+            gs.BorderBrush = new SolidColorBrush(Colors.Black);
+            Grid.SetRow(gs, (dashboard.RowDefinitions.Count-1));
+            Grid.SetColumnSpan(gs, 20);
+            dashboard.Children.Add(gs);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+            if (cellindex == 3)
+                MessageBox.Show("Please add new row ", "yes");
+            else
+            {
+                Rectangle ie = new Rectangle();
+                ie.Fill = new SolidColorBrush(Colors.Blue);
+                //WebBrowser ie = new WebBrowser();
+                ie.Margin= new Thickness(2, 2, 2, 2);
+                Grid.SetRow(ie, (dashboard.RowDefinitions.Count-1));
+                Grid.SetColumn(ie, cellindex);
+                dashboard.Children.Add(ie);
+                cellindex++;
+            }
         }
     }
 }
