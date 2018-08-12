6621769a21c73cbec4b0d224f451ee8eac0afe9f
ahmed-abdelsalam <engahmedabdelsalam918@gmail.com>
2018-08-12 21:02:25 +0200

    some fixes

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -12,10 +12,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CefSharp.Wpf;
+using CefSharp;
 
 namespace WpfApp2
 {
-    using System.Windows.Media;
+    
 
 
     /// <summary>
@@ -27,7 +29,11 @@ namespace WpfApp2
         public MainWindow()
         {
             InitializeComponent();
+            w11.UseLayoutRounding = true;
+              
         }
+        
+        
 
         public static T FindChild<T>(DependencyObject parent, string childName)
    where T : DependencyObject
@@ -74,7 +80,7 @@ namespace WpfApp2
         }
 
 
-
+      
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -125,15 +131,90 @@ namespace WpfApp2
             {
                 var name = "w" + index.Text.ToString();
                 WebBrowser web = FindChild<WebBrowser>(Application.Current.MainWindow, name);
-               // WebBrowser web = (WebBrowser)dashboard.FindName(name);
+                // WebBrowser web = (WebBrowser)dashboard.FindName(name);
                 web.Navigate(url.Text.ToString());
 
             }
-        }
+            if (e.Key == Key.F)
+            {
+                var name = "w11";
+                WebBrowser web = FindChild<WebBrowser>(Application.Current.MainWindow, name); 
+                web.Height = scroll.ActualHeight;
+                web.Width = scroll.ActualWidth;
+               
 
+            }
+            if (e.Key == Key.M)
+            {
+                var name = "w11";
+                WebBrowser web = FindChild<WebBrowser>(Application.Current.MainWindow, name);
+                web.Height = Double.NaN;
+                web.Width = Double.NaN;
+                dashboard.Visibility = System.Windows.Visibility.Visible;
+
+            }
+        }
         private void TextBox_TouchEnter(object sender, TouchEventArgs e)
         {
 
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            cellindex++;
+            var currentcol = Grid.GetColumn(add);
+            var currentrow= Grid.GetRow(add);
+           
+
+            if (cellindex >= 3)
+            {
+                cellindex = 0;
+                GridLength heigh = new GridLength(290);
+                RowDefinition newrow = new RowDefinition();
+                newrow.Height = heigh;
+                dashboard.RowDefinitions.Add(newrow);
+
+
+
+                GridSplitter gs = new GridSplitter();
+                gs.VerticalAlignment = System.Windows.VerticalAlignment.Bottom;
+                gs.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+                gs.Height = 8;
+                gs.ResizeDirection = System.Windows.Controls.GridResizeDirection.Rows;
+                gs.BorderThickness = new Thickness(3);
+                gs.BorderBrush = new SolidColorBrush(Colors.Black);
+                Grid.SetRow(gs, (dashboard.RowDefinitions.Count - 1));
+                Grid.SetColumnSpan(gs, 20);
+                dashboard.Children.Add(gs);
+                Grid.SetRow(add, (dashboard.RowDefinitions.Count - 1));
+                Grid.SetColumn(add, 0);
+            }
+            else
+            Grid.SetColumn(add, currentcol + 1);
+            
+            ChromiumWebBrowser web = new ChromiumWebBrowser();
+            web.Margin= new Thickness(10);
+            web.Name = "w" + (currentrow + 1).ToString() + (cellindex + 1);
+            web.Address="https://google.com";
+            Grid.SetRow(web, currentrow);
+            Grid.SetColumn(web, currentcol);
+            dashboard.Children.Add(web);
+            
+        }
+
+        private void menu_Click(object sender, RoutedEventArgs e)
+        {
+            if (popstack.Width==30)
+            {
+                scroll.Margin = new Thickness(100, 0, 0, 0);
+                popstack.Width = 100;
+            }
+            else
+            {
+                scroll.Margin = new Thickness(30, 0, 0, 0);
+                popstack.Width = 30;
+
+            }
         }
     }
 }
